9d320316eaa12b0cbbf12f558477988cbb2b9997
Juan Vicente Vazquez <dev@juanvicentevazquezg.es>
2023-11-02 11:23:00 +0100

    Searchbox, sidebar y gifServices

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html'
 })
 
-export class SearchBoxComponent implements OnInit {
-  constructor() { }
+export class SearchBoxComponent  {
+  constructor(public gifService: GifsService) { }
 
-  ngOnInit() { }
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>
+
+  searchTag( ): void{
+    const newTag = this.tagInput.nativeElement;
+    this.gifService.searchTag(newTag.value)
+    this.tagInput.nativeElement.value = '';
+  }
+
 }
